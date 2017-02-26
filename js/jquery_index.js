3eb9d653022a9908959a0bf275277bb5df022462
huaral <huaral@163.com>
2017-02-26 14:01:16 +0800

    init project

---
+++ js/jquery_index.js
@@ -0,0 +1,126 @@
+{
+	$(document).ready(function() {
+		
+
+//		$.ajax({
+//			type: "get",
+//			url: "http://huaral.tech:9000/huaralserver/index.php",
+//			async: true,
+////			data: {
+////				'url': "url"
+////			}
+//			dataType : "JSON",
+//			success: function(data, status, xhl) {
+//				console.log(data);
+//				console.log(data.msg_code);
+//				var arrs = data.msg_data;
+//				console.log(arrs);
+//				var siderbar = $(".sidebar");
+//				var div = siderbar.find("div").clone(false);
+//				for(var i = 0;i<arrs.length;i++){
+//					console.log(arrs[i].bookname);
+//					var dom = div.clone(false);
+//					dom.text(arrs[i].bookname);
+//					siderbar.append(dom);
+//				}
+//			}
+//		});
+	tagsArr = document.getElementsByClassName('nav_tag');
+	$('.nav_tag').click(function(){
+		var siderbar = $(".sidebar");
+		siderbar.empty();
+		var bookname = "";
+		switch(this){
+			case tagsArr[0]:
+				
+			break;
+			case tagsArr[1]:
+				bookname = "ios";
+			break;
+			case tagsArr[2]:
+				bookname = "android";
+			break;
+			case tagsArr[3]:
+				bookname = "html5";
+			break;
+			case tagsArr[4]:
+				bookname = "javaee";
+			break;
+			case tagsArr[5]:
+				bookname = "php";
+			break;
+			case tagsArr[6]:
+				bookname = "aboutours";
+			break;
+		}
+		console.log("afsdfadfasd");
+		$.ajax({
+			type: "get",
+			url: "http://127.0.0.1/huaralserver/interface/Booktags.php",
+			async: true,
+			data: {
+				'bookname': bookname
+			}
+			,
+			dataType : "JSON",
+			success: function(data, status, xhl) {
+				var arrs = data.msg_data[0].booktags;
+				console.log(arrs);
+				var siderbar = $(".sidebar");
+				var box = $('<div class="bookid"></div>');
+				box.css({
+					'height': '34px',
+					'font-size': '13px',
+					'color': '#333',
+					'vertical-align': 'middle',
+					'line-height':'34px',
+					'cursor': 'pointer',
+//					'border-radius': '5px'
+				});
+				selected = 0;
+				for(var i = 0;i< arrs.length;i++){
+					console.log(arrs[i]);
+					var dom = box.clone(true);
+					if(i == 0){
+						dom.css({
+							'background-color':'#FFF'
+						});
+					}
+					dom.text(arrs[i]);
+					dom[0].index = i;
+					siderbar.append(dom);
+					dom.click(function(){
+						$('.bookid')[selected].style.background ='#DCDCDC';
+						selected = this.index;
+//						alert(selected);
+					});
+					dom.hover(function(){
+						$(this).css({
+							'background-color':'#FFF'
+							});	
+					},function(){
+//						alert(this.index);
+						if(selected!=this.index){
+							$(this).css({
+							'background-color':'#DCDCDC'
+							});
+						}else{
+							$(this).css({
+							'background-color':'#FFF'
+							});
+						}
+				});
+				}
+			},
+			error:function(data){
+			
+			}
+
+		});
+		
+	});
+
+
+	});
+
+}
---
+++ js/js_index.js
@@ -0,0 +1,102 @@
+{
+
+	var t = n = 0,
+		count;
+	$(document).ready(function() {
+			// 获取图片标签长度
+			count = $("#banner_list a").length;
+			// 不是当前显示的图片隐藏
+			$("#banner_list a:not(:first-child)").hide();
+			// 点击下面的1234按钮，切换图片
+			$("#banner li").click(function() {
+				var i = $(this).text() - 1;
+				n = i;
+				if(i >= count) return;
+				// 淡入淡出效果
+				$("#banner_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
+				// 响应点击
+				$(this).toggleClass("on");
+				// 切换按钮的时候让上一个按钮回归原来的样式
+				$(this).siblings().removeAttr("class");
+			});
+			t = setInterval("showAuto()", 6000);
+			// $("#banner").hover(function() {
+			//         clearInterval(t)
+			//     },
+			//     function() {
+			//         t = setInterval("showAuto()", 3000);
+			//     });
+		})
+		// 自动轮播
+	function showAuto() {
+		n = n >= (count - 1) ? 0 : ++n;
+		$("#banner li").eq(n).trigger('click');
+	}
+	var searchState = 0;
+	window.onload = function() {
+		//search
+		document.getElementById('searchBtn').onclick = function() {
+				var search = document.getElementById('searchdiv');
+				search.style.visibility = (searchState ? "hidden" : "visible")
+				searchState = !searchState;
+				document.getElementById('searchValue').focus();
+				var rightBar = document.getElementById('rightBar');
+
+		};
+			//rightBar
+		var rightBarState = 0;
+		document.getElementById('rightBarBtn').onclick = function() {
+			var rightBar = document.getElementById('rightBar');
+			//  			rightBar.style.marginLeft = rightBarState?'92%':'100%';
+			//  			rightBarState = !rightBarState;
+			rightBar.style.animationDuration = '0.7s';
+			rightBar.style.animationFillMode = 'forwards';
+			if(rightBarState) {
+				rightBar.style.animationName = 'rightBarToRight';
+				console.log("i am coming");
+				if(isIE()){
+					rightBar.style.marginLeft = '100%';
+				}
+
+			} else {
+				rightBar.style.animationName = 'rightBarToLeft';
+				console.log("i am coming");
+				if(isIE()){
+					rightBar.style.marginLeft = '92%';
+				}
+			}
+			rightBarState = !rightBarState;
+		};
+		function isIE() { //ie?
+			if(!!window.ActiveXObject || "ActiveXObject" in window)
+				return true;
+			else
+				return false;
+		}
+		//点击navgationBarTag
+		var tagState = 0;
+		var tagsArr = new Array();
+		tagsArr = document.getElementsByClassName('nav_tag');
+		var nav_tag = tagsArr[0];
+		nav_tag.style.backgroundColor = '#000';
+		for(var i = 0;i<tagsArr.length;i++){
+			var tag = tagsArr[i];
+			tag.index = i;
+			tag.onclick = function(){
+				if(this.index >0){
+//					alert("进来了1");
+					if(!tagState){
+						tagState = 1;
+					}
+				}else{
+					tagState = 0;
+				}
+				document.getElementById('tag_content').style.display = (tagState ? 'block' : 'none');
+				document.getElementById('index_content').style.display = (tagState ? 'none' : 'block');
+				nav_tag.style.backgroundColor = '#333';
+				this.style.backgroundColor = '#000';
+				nav_tag = this;
+			}
+		}
+	}
+}
